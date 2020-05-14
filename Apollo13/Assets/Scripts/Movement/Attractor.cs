af25e489d7b416165021c4cfa22219770c98e374
Connor <cph5236@psu.edu>
2020-05-14 01:46:35 -0400

    Basic Gravity Setup and ship wasd movement for testing.

---
+++ Apollo13/Assets/Scripts/Movement/Attractor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+    private float G = 5;
+    public Rigidbody2D rb;
+
+    private void FixedUpdate()
+    {
+        Attractor[] attractors = FindObjectsOfType<Attractor>();
+        foreach(Attractor attractor in attractors)
+        {
+            if (attractor != this)
+                Attract(attractor);
+        }
+    }
+
+    void Attract(Attractor objToAttract)
+    {
+        Rigidbody2D rbToAttract = objToAttract.rb;
+
+        Vector2 direction = rb.position - rbToAttract.position;
+        float dist = direction.magnitude;
+
+        float forceMag = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(dist, 2);
+
+        Vector2 force = direction.normalized * forceMag;
+
+        rbToAttract.AddForce(force);
+    }
+}
