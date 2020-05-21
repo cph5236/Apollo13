1850f9e9240145ec5e90b9446d9e95c8f2e9310a
apio9090 <apio9009@gmail.com>
2020-05-20 21:17:27 -0400

    Changed Ship Movement

---
+++ Apollo13/Assets/Scripts/EndScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("fuck You1"); ///use this one
+    }
+
+}
